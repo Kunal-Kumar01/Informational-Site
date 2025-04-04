656024ac544b2926eb9b0dc4a82929af942c994f
Kunal Kumar <kunal.lohano@gmail.com>
2025-04-04 23:50:10 +0500

    Create Server

---
+++ Server.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const fs = require('fs')
+
+const port = 8080;
+
+
+const server = http.createServer((req, res)=>{
+    res.setHeader('Content-Type', 'text/html')
+
+    if(req.url=='/'){
+        res.statusCode=200;
+        let page = fs.readFileSync('index.html');
+        res.end(page.toString())
+    }
+    else if(req.url=='/about'){
+        res.statusCode=200;
+        let page = fs.readFileSync('about.html');
+        res.end(page.toString())
+    }
+    else if(req.url=='/contact-me'){
+        res.statusCode=200
+        let page = fs.readFileSync('contact-me.html')
+        res.end(page.toString())
+    }
+    else{
+        res.statusCode = 404
+        let page = fs.readFileSync('404.html')
+        res.end(page.toString())
+    }
+})
+
+server.listen(port, ()=>{
+    console.log(`Server listening to port: ${port}`)
+})
